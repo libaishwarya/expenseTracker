ec0c59ece76bb207fa3783120d007fc526001d25
Aishwarya M <104068306+libaishwarya@users.noreply.github.com>
2023-10-31 22:02:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, request, jsonify , render_template
 import mysql.connector 
 app = Flask(__name__)
 
@@ -9,81 +9,62 @@ db_config = {
     'database': 'expenseTracker'
 }
 
-@app.route('/add_salary', methods=['POST'])
+@app.route('/add_salary', methods=['GET','POST'])
 def add_salary():
-    try:
-        data = request.json
-        description = data['description']
-        amount = float(data['amount'])
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO salary (description, amount) VALUES (%s,%s)', (description, amount))
-        conn.commit()
-        conn.close()
-        return jsonify(message='Salary added successfully'), 201
-    except Exception as e:
-        return jsonify(error=str(e)), 400
+    if request.method == 'POST':
+        try:
+            data = request.form
+            description = data['description']
+            amount = float(data['amount'])
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO salary (description, amount) VALUES (%s,%s)', (description, amount))
+            conn.commit()
+            conn.close()
+            return jsonify(message='Salary added successfully'), 201
+        except Exception as e:
+            return jsonify(error=str(e)), 400
+    return render_template ('addSalary.html')
 
-@app.route('/add_expense', methods=['POST'])
+@app.route('/add_expense', methods=['GET','POST'])
 def add_expense():
-    try:
-        data = request.json
-        description = data['description']
-        amount = float(data['amount'])
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO expenses (description, amount) VALUES (%s, %s)', (description, amount))
-        conn.commit()
-        conn.close()
-        return jsonify(message='Expense added successfully'), 201
-    except Exception as e:
-        return jsonify(error=str(e)), 400
+    if request.method == 'POST':
+        try:
+            data = request.form
+            description = data['description']
+            amount = float(data['amount'])
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO expenses (description, amount) VALUES (%s, %s)', (description, amount))
+            conn.commit()
+            conn.close()
+            return jsonify(message='Expense added successfully'), 201
+        except Exception as e:
+            return jsonify(error=str(e)), 400
+    return render_template ('addExpense.html')
 
-@app.route('/calculate_balance', methods=['GET'])
-# def calculate_balance():
-#     try:
-#         conn = mysql.connector.connect(**db_config)
-#         cursor = conn.cursor()
-#         cursor.execute('SELECT SUM(amount) FROM salary')
-#         # salary_total = cursor.fetchone()[0] or 0
-#         result_1 = cursor.fetchone()
-#         if result_1 is not None:
-#             salary_total = result_1[0]
-#         else:
-#             salary_total = 0
-
-#         cursor.execute('SELECT SUM(amount) FROM expenses')
-#         # expenses_total = cursor.fetchone()[0] or 0
-#         result_2 = cursor.fetchone()
-#         if result_2 is not None:
-#             expenses_total = result_2[0]
-#         else:
-#             expenses_total = 0
-
-#         balance = salary_total - expenses_total
-
-#         conn.close()
-#         return jsonify(salary_total=salary_total, expenses_total=expenses_total, balance=balance)
-#     except Exception as e:
-#         return jsonify(error=str(e)), 500
-
+@app.route('/calculate_balance', methods=['GET', 'POST'])
 def calculate_balance():
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
+    if request.method == "GET":
+        try:
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
 
-        cursor.execute('SELECT IFNULL(SUM(amount), 0) FROM salary')
-        salary_total = cursor.fetchone()[0]
+            cursor.execute('SELECT IFNULL(SUM(amount), 0) FROM salary')
+            salary_total = cursor.fetchone()[0]
 
-        cursor.execute('SELECT IFNULL(SUM(amount), 0) FROM expenses')
-        expenses_total = cursor.fetchone()[0]
+            cursor.execute('SELECT IFNULL(SUM(amount), 0) FROM expenses')
+            expenses_total = cursor.fetchone()[0]
 
-        balance = salary_total - expenses_total
+            balance = salary_total - expenses_total
 
-        conn.close()
-        return jsonify(salary_total=salary_total, expenses_total=expenses_total, balance=balance)
-    except Exception as e:
-        return jsonify(error=str(e)), 500
+            conn.close()
+            return jsonify(salary_total=salary_total, expenses_total=expenses_total, balance=balance)
+        except Exception as e:
+            return jsonify(error=str(e)), 500
+    return render_template ('balanceAmount.html', balance_amount = balance)
+        
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='192.168.0.135', port=5000)
